ae27cfee7986ffc71986f37b016ab68551e142d4
𝕋𝕣𝕒𝕚𝕟 𝔾𝕦𝕪 𝟝𝟛 <98115896+trainguy53@users.noreply.github.com>
2025-07-17 23:36:09 +0200

    Update js.js

--- js.js
+++ js.js
@@ -4,7 +4,6 @@
 //                                                                                                                   //
 // ================================================================================================================= //
 
-
 const titrePageParent = window.parent.document.title;
 const pages = {
 	"Accueil — Train Guy 53": "index.html",
@@ -15,36 +14,16 @@ const pages = {
 	"Rails — Train Guy 53": "rails.html"
 };
 
-const urlHtml = "https://validator.w3.org/nu/?doc=https%3A%2F%2Ftrainguy53.github.io%2Freseau%2F";
-const urlCss = "https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Ftrainguy53.github.io%2Freseau";
-
-function validation(langage) {
-
-	switch (langage) {
-		case "html":
-			if (titrePageParent in pages) {window.open(urlHtml + pages[titrePageParent], "_blank")}
-			break;
-		case "css":
-			window.open(urlCss, "_blank");
-			break;
-	}
-
-}
-
 function cacherImagesInternet() {
-
 	const imgInternet = document.querySelectorAll(".img-internet");
 	imgInternet.forEach(img => img.style.display = "none");
-
 }
 
 function basculerImages() {
-
 	const imgInternet = document.querySelectorAll(".img-internet");
 	const imgRéseau = document.querySelectorAll(".img-réseau");
 	imgRéseau.forEach(img => img.style.display = img.style.display === "none" ? "block" : "none");
 	imgInternet.forEach(img => img.style.display = img.style.display === "none" ? "block" : "none");
-
 }
 
 // ================================================================================================================= //
